8051d5af82b9be0d43733daf9e21656f763205e2
ruilong <gzwuruilong@gmail.com>
2022-07-21 17:33:27 +0800

    Add files via upload

---
+++ v3sdk/src/model/Card.php
@@ -0,0 +1,18 @@
+<?php
+namespace lianlianpay\v3sdk\model;
+
+class Card
+{
+    public $holder_name;
+    public $id_type;
+    public $id_no;
+    public $card_no;
+    public $card_type;
+    public $bank_code;
+    public $card_brand;
+    public $card_expiration_year;
+    public $card_expiration_month;
+    public $cvv;
+    public $card_token;
+    public $billing_address;
+}
